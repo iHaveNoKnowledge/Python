66d993b6b1eb9e076ce38cabb1ea283df1b89082
iHaveNoKnowledge <hackdk12@gmail.com>
2023-09-09 10:22:56 +0700

    update passworld
    [Update] the old password has already deprecated

--- autopage.py
+++ autopage.py
@@ -552,7 +552,7 @@ try:
         driver.find_element(
             By().XPATH, '/html/body/div[1]/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/div[2]/input').send_keys("62078")
         driver.find_element(
-            By().XPATH, '/html/body/div[1]/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/div[3]/input').send_keys("ITcity@2022")
+            By().XPATH, '/html/body/div[1]/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/div[3]/input').send_keys("ITcity@2017")
         driver.find_element(
             By().XPATH, '/html/body/div[1]/div[2]/div[2]/div[6]/div/div/div[2]/div[5]/div/textarea').send_keys("Online")
 
