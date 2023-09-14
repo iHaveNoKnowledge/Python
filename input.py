3afa25eaaff023826abe26f35dc84488f9a52b78
iHaveNoKnowledge <hackdk12@gmail.com>
2023-09-14 17:57:11 +0700

    update

--- input.py
+++ input.py
@@ -1,2 +1,10 @@
+import pandas as pd
+
+file_path = r"C:\Users\ONLINE_MIS\Desktop\Trans-am 31-01-2022\Projects\python\Python\test_pandas01\Order.toship.20230903_20230914 (1).xlsx"
+data_frame = pd.read_excel(file_path)
+
 order_input = input("Order?: ")
 print("Order is: ", order_input)
+
+filter_data = data_frame["หมายเลขคำสั่งซื้อ"] == order_input
+print("find in data_frame: ", filter_data[0])
--- test_pandas01.py
+++ test_pandas01.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
-file_path = './test_pandas01/Order.toship.20230820_20230826.xlsx'
+file_path = './test_pandas01/Order.toship.20230903_20230914 (1).xlsx'
 data_frame = pd.read_excel(file_path)
-filtered_data = data_frame[(data_frame['หมายเลขคำสั่งซื้อ'] == '230823EJW39APX')] 
-order_status = filtered_data['สถานะการสั่งซื้อ'][0]
+filtered_data = data_frame[(
+    data_frame['หมายเลขคำสั่งซื้อ'] == '230909SXC7FH38')]
+order_status = filtered_data['สถานะการสั่งซื้อ']
+products = filtered_data['เลขอ้างอิง SKU (SKU Reference No.)']
 # print(filtered_data[['หมายเลขคำสั่งซื้อ','สถานะการสั่งซื้อ']])
 print(filtered_data)
 print(order_status)
 print(order_status == 'ที่ต้องจัดส่ง')
 
+print("ซื้อไรบ้าง")
+print(products)
+
 # if order_status == 'ที่ต้องจัดส่ง':
 #     print("ต้องส่ง")
 # else :
 #     print("ยกเลิก")
-    
+
 # items = ("ตู่", "ป้อม", "เต้")
 # idx = [10,1,2]
 # x = pd.Series(items)
@@ -22,4 +27,3 @@ print(order_status == 'ที่ต้องจัดส่ง')
 # colors= {"green":"เขียว", "char2":"ลุงตู่", "char3":"ลุงป้อม"}
 # dict_x = pd.Series(colors)
 # print(f"""{dict_x.to_frame()}""")
-
