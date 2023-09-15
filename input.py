3370b2dee0cbe950c60fe00f2708abae62964e1e
iHaveNoKnowledge <hackdk12@gmail.com>
2023-09-16 05:11:59 +0700

    Update input.py

--- input.py
+++ input.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # * เตรียม data frame
-file_path = r"C:\Users\ONLINE_MIS\Desktop\Trans-am 31-01-2022\Projects\python\Python\test_pandas01\Order.toship.20230903_20230914 (1).xlsx"
+file_path = r"./test_pandas01/Order.toship.20230903_20230914 (1).xlsx"
 data_frame = pd.read_excel(file_path)
 
 # * เลือก Order
@@ -9,11 +9,19 @@ order_input = input("Order?: ")
 print("Order is: ", order_input)
 filter_data = data_frame[(data_frame["หมายเลขคำสั่งซื้อ"]
                           == order_input)]
-bool_filter = data_frame["หมายเลขคำสั่งซื้อ"] == order_input
-print("boolfilter มี type เป็นไร", type(bool_filter))
-print("ใช้ boolfilter", data_frame[bool_filter])
+filtered_row = data_frame["หมายเลขคำสั่งซื้อ"] == order_input
+print("boolfilter มี type เป็นไร", type(filtered_row))
+print("ใช้ boolfilter", data_frame[filtered_row]['สถานะการสั่งซื้อ'])
 # print("find in data_frame: ", filter_data)
 
-# * เลือก Column มาแสดงผล
-# order_status = filter_data["สถานะการสั่งซื้อ"]
-# print(order_input, "มีสถานะ: ", order_status)
+#* ประเภทใบกำกับภาษี
+# * เลือก Column มาแสดงผล โดยการใช้ iloc[0]
+if data_frame[filtered_row]['ประเภทใบกำกับภาษี HHเป็นตัวบอกHH'].iloc[0] == 'Personal':
+    tax_bool = False
+else :
+    tax_bool = True
+
+
+
+#* แสดงผล
+print("ใบกำกับ?", tax_bool)
