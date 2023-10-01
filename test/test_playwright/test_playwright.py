2b1e6776be1796edf1473dc5fb388d1369d2b71b
iHaveNoKnowledge <hackdk12@gmail.com>
2023-10-01 20:47:31 +0700

    test

--- test/test_playwright/test_playwright.py
+++ test/test_playwright/test_playwright.py
@@ -1,4 +1,10 @@
-from playwright.sync import sync_playwright
+import re
+from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch()
+    browser = p.chromium.launch(headless=False, slow_mo=3000)
+    page = browser.new_page()
+    page.goto('https://google.com')
+    print(page.title())
+
+
