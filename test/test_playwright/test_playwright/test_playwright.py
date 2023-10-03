320b5df5ac43ab998ee6d228d63bf725b4c99b4d
iHaveNoKnowledge <hackdk12@gmail.com>
2023-10-03 17:46:15 +0700

    Update test_playwright.py

--- test/test_playwright/test_playwright/test_playwright.py
+++ test/test_playwright/test_playwright/test_playwright.py
@@ -1,19 +1,14 @@
-import re
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
 
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False, slow_mo=1000)
-#     page = browser.new_page()
-#     page.goto('https://google.com')
-#     page.screenshot(path="ex.png")
-#     print(page.title())
+async def main():
+    playwright = await async_playwright().start()
+    browser = await playwright.chromium.launch()
+    page = await browser.new_page()
+    await page.goto("https://playwright.dev/")
+    await page.screenshot(path="example.png")
+    await browser.close()
+    await playwright.stop()
 
-playwright = sync_playwright().start()
-# Use playwright.chromium, playwright.firefox or playwright.webkit
-# Pass headless=False to launch() to see the browser UI
-browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-page = browser.new_page()
-page.goto("https://playwright.dev/")
-
-# browser.close()
-# playwright.stop()
+if __name__ == "__main__":
+    asyncio.run(main())
