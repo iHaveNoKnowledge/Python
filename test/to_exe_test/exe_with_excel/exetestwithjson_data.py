5d86380dc47f460fbd50d62bc2288ae16b5e5eb7
iHaveNoKnowledge <hackdk12@gmail.com>
2023-10-04 22:13:04 +0700

    save

--- test/to_exe_test/exe_with_excel/exetestwithjson_data.py
+++ test/to_exe_test/exe_with_excel/exetestwithjson_data.py
@@ -1,11 +1,21 @@
 import json
 import os
 
+import os
+# file_path = os.path.join('test', 'to_exe_test', 'exe_with_excel', 'thai_address_pattern.json')
 
-current_dir = os.getcwd()
-print("ตอนนี้: ", current_dir)
-
-
-with open('thai_address_pattern.json',"r", encoding="utf-8") as file:
-    data = json.load(file)
-print(data[0:2])
+# file_path = 'test/to_exe_test/exe_with_excel/thai_address_pattern.json'
+file_path= 'thai_address_pattern.json'
+try:
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+ 
+    print("ก่อนใช้ print(data[0:2]) ")
+ 
+    print(data[0:2])
+  
+    print("หลังจากใช้ print(data[0:2]) ")
+   
+except Exception as err:
+    print("พัง", err)
+input("Press Enter to end the script")
