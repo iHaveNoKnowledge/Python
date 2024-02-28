b9485361ba247d68de62faa80e9941a6375333c9
iHaveNoKnowledge <99856000+iHaveNoKnowledge@users.noreply.github.com>
2024-02-28 12:11:02 +0700

    save

--- projects/auto_sn/auto_sn.py
+++ projects/auto_sn/auto_sn.py
@@ -4,6 +4,42 @@ import tkinter as tk
 class MainApp(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
+        self.canvas = tk.Canvas(self.master, borderwidth=1, highlightthickness=1)
+        self.scrollbar = tk.Scrollbar(
+            self, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = tk.Frame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox('all')
+            )
+        )
+
+        self.canvas.create_window(
+            (0, 0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
+        self.scrollable_frame.bind("<Enter>", self._bind_mousewheel)
+        self.scrollable_frame.bind("<Leave>", self._unbind_mousewheel)
+        
+        for i in range(50):
+            tk.Label(self.scrollable_frame, text=f"label {i}").pack()
+        
+        
+
+    def _bind_mousewheel(self, event):
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _unbind_mousewheel(self, event):
+        self.canvas.unbind_all("<MouseWheel>")
+
+    def _on_mousewheel(self, event):
+        self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
         print("start MainApp")
 
 
