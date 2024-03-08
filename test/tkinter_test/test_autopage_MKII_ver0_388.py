66729e394bf9e366fa8f7a2ab65721fe409d971c
iHaveNoKnowledge <99856000+iHaveNoKnowledge@users.noreply.github.com>
2024-03-08 10:21:20 +0700

    update version

--- test/tkinter_test/test_autopage_MKII_ver0_388.py
+++ test/tkinter_test/test_autopage_MKII_ver0_388.py
@@ -123,7 +123,7 @@ class MyApp:
 
     def create_main_window(self):
         self.root.geometry("1000x900+400+300")
-        self.root.title("Autosamatic ver0.387")
+        self.root.title("Autosamatic ver0.388")
         self.root.configure(bg="#444")
 
         # #* BG CANVAS ##################################################################################
