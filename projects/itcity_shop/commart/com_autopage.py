bbeee98cae42a965eeb3d607311516a4cb3f7bb4
iHaveNoKnowledge <99856000+iHaveNoKnowledge@users.noreply.github.com>
2024-07-02 02:09:11 +0700

    [Progress] แยก Thread

--- projects/itcity_shop/commart/com_autopage.py
+++ projects/itcity_shop/commart/com_autopage.py
@@ -1,43 +1,35 @@
 import tkinter as tk
 from tkinter import filedialog
-from tkinter import ttk
 from customtkinter import *
-
-from modules.selenium_webdriver import ChromeDriver
-# from modules.unified_table_data import UnifyData
-
-import traceback
+import threading
+import time
 
 
 class MainApp:
     def __init__(self, root):
         self.root = root
-        # * for query
-        # self.data_file_dir = ''
-        self.data_file_dir = StringVar(value="")
-        self.file_name = StringVar(value="ไม่มีไฟล์")
-        self.sku = StringVar(value="")
-        self.set_num = StringVar(value="")
-        # * เอาไว้เก็บ object ที่รับค่าจาก excel, import file, อะไรก็ตามที่เป็น data table ที่นำเข้ามา
+        self.data_file_dir = tk.StringVar(value="")
+        self.file_name = tk.StringVar(value="ไม่มีไฟล์")
+        self.sku = tk.StringVar(value="")
+        self.set_num = tk.StringVar(value="")
+        self.current_task = None
+        self.stop_event = threading.Event()
         self.data_table = {}
         self.create_main_window()
-        # ใช้ class unified
-        # self.chromdriver_controller = ChromeDriver()
 
     def resetAllValue(self):
-        self.data_file_dir.set(value="")
-        self.sku.set(value="")
-        self.set_num.set(value="")
+        self.data_file_dir.set("")
+        self.sku.set("")
+        self.set_num.set("")
 
     def resetInput(self):
-        self.sku.set(value="")
-        self.set_num.set(value="")
+        self.sku.set("")
+        self.set_num.set("")
 
     def add_file(self):
         self.data_file_dir.set('')
         self.data_file_dir.set(filedialog.askopenfilename(
             title="Select an import file"))
-        # * ตัดเอาเฉพาะ ชื่อไฟล์
         if self.data_file_dir.get():
             self.file_name.set(f"{self.data_file_dir.get().split('/')[-1]}")
             print('มีชื่อไฟล์', self.file_name.get())
@@ -52,15 +44,11 @@ class MainApp:
             self.resetAllValue()
             print("ไม่มีชื่อไฟล์", self.data_file_dir.get())
 
-            pass
-
     def search(self):
-        # * ลบ result products list เก่า
         try:
             sku = self.sku.get().strip()
             set = self.set_num.get().strip()
 
-            # * เกี่ยวกับการแสดงผล GUI
             self.clear_log()
             data_dict = self.data_table.get_result(sku, set)
             self.update_log(f"ชื่อชุด: {sku}")
@@ -68,7 +56,6 @@ class MainApp:
             self.sku.set("")
             self.set_num.set("")
 
-            # todo Continue from here// wip you are here
             try:
                 self.chromdriver_controller.operation_start(sku, data_dict)
             except ValueError as e:
@@ -78,156 +65,99 @@ class MainApp:
             self.update_log("พัง")
             raise ValueError('search พัง: ', traceback.format_exc())
 
+    def my_function(self):
+        print("Starting function")
+        try:
+            for i in range(10):
+                if self.stop_event.is_set():
+                    print("Function was stopped")
+                    return
+                time.sleep(1)
+                print(f"Running... {i+1}/10")
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return
+        print("Function completed")
+
+    def start_task(self, input={}):
+        if self.current_task and self.current_task.is_alive():
+            self.stop_event.set()
+            self.current_task.join()
+        self.stop_event.clear()
+        self.current_task = threading.Thread(target=self.my_function)
+        self.current_task.start()
+        print("เริ่มการทำงาน")
+        print("input: ", input)
+
     def update_log(self, update_txt=""):
         self.update_txt = update_txt
-        self.log_display.config(state=NORMAL)
-        self.log_display.insert(END, self.update_txt + "\n")
-        self.log_display.config(state=DISABLED)
+        self.log_display.config(state=tk.NORMAL)
+        self.log_display.insert(tk.END, self.update_txt + "\n")
+        self.log_display.config(state=tk.DISABLED)
 
     def clear_log(self):
         print('เคลีย!!')
-        self.log_display.config(state=NORMAL)
-        self.log_display.delete("1.0", END)
-        self.log_display.config(state=DISABLED)
+        self.log_display.config(state=tk.NORMAL)
+        self.log_display.delete("1.0", tk.END)
+        self.log_display.config(state=tk.DISABLED)
 
     def create_widgets(self):
-        # * Variables for setting options
         self.pady = (0, 16)
 
-        # * Frame Top Section /////////////////////////////////////////////////////////////////////////////
-        # * > QR Input Component
-        # * >> Labels
         self.qr_display = CTkLabel(
-            self.frame_top, text="QR Input",
-            width=70,
-            anchor=W
-        )
-        self.qr_display.grid(
-            row=0, column=0, padx=(0, 0),  pady=self.pady
-        )
-        # * >> Inputs
-        self.qr_input = CTkEntry(
-            self.frame_top,
-            width=180,
-        )
+            self.frame_top, text="QR Input", width=70, anchor=tk.W)
+        self.qr_display.grid(row=0, column=0, padx=(0, 0), pady=self.pady)
+
+        self.qr_input = CTkEntry(self.frame_top, width=180)
         self.qr_input.grid(row=0, column=1, pady=self.pady, sticky="w")
-        # * >> Button
-        self.qr_btn = CTkButton(
-            self.frame_top,
-            width=70,
-            anchor=W,
-            text=f"Start",
-            command=self.add_file,
-        )
+
+        self.qr_btn = CTkButton(self.frame_top, width=70, anchor=tk.W,
+                                text="Start", command=self.on_start_button_click)
         self.qr_btn.grid(row=0, column=2, padx=(
             0, 0), pady=self.pady, sticky="w")
 
-        # * > Order display Conponent
-        # * >> Label
         self.order_label = CTkLabel(
-            self.frame_top,
-            text="เลขที่คำสั่งซื้อ",
-            width=70,
-            anchor=W
-        )
+            self.frame_top, text="เลขที่คำสั่งซื้อ", width=70, anchor=tk.W)
         self.order_label.grid(row=0, column=3, padx=(10, 10), pady=self.pady)
-        # * >> Display
+
         self.order_display = CTkEntry(
-            self.frame_top,
-            width=180,
-            state="readonly",
-        )
-        self.order_display.grid(row=0, column=4, padx=(
-            1, 1), pady=self.pady)
+            self.frame_top, width=180, state="readonly")
+        self.order_display.grid(row=0, column=4, padx=(1, 1), pady=self.pady)
 
-        # * > Name Display component
-        # * >> Label
         self.name_label = CTkLabel(
-            self.frame_top,
-            text="ชื่อออกบิล",
-            width=70,
-            anchor=W,
-        )
+            self.frame_top, text="ชื่อออกบิล", width=70, anchor=tk.W)
         self.name_label.grid(row=1, column=0, padx=(0, 0))
-        # * >> Display
-        self.name_display = CTkEntry(
-            self.frame_top,
-            width=300,
-            # state="readonly",
 
-        )
+        self.name_display = CTkEntry(self.frame_top, width=300)
         self.name_display.grid(row=1, column=1, padx=(0, 1))
 
-        # * > TaxNum display Conponent
-        # * >> Label
         self.tax_num_label = CTkLabel(
-            self.frame_top,
-            text="เลขผู้เสียภาษี",
-            width=70,
-            anchor=W,
-
-        )
+            self.frame_top, text="เลขผู้เสียภาษี", width=70, anchor=tk.W)
         self.tax_num_label.grid(row=1, column=3, padx=(10, 10))
-        # * >> Display
-        self.tax_num_display = CTkEntry(
-            self.frame_top,
-            width=180,
-            state="readonly",
 
-        )
+        self.tax_num_display = CTkEntry(
+            self.frame_top, width=180, state="readonly")
         self.tax_num_display.grid(row=1, column=4, padx=(0, 1))
 
-        # * Frame1 Section /////////////////////////////////////////////////////////////////////////////
-        # * > address display Conponent /////////////////////
-        # * >> Label
         self.address_label = CTkLabel(
-            self.frame_1,
-            text="ที่อยู่ ",
-            width=70,
-            anchor=W
-        )
-        self.address_label.grid(row=0, column=0, padx=(0, 0),
-                                pady=(0, 5), sticky='nw')
-        # * >> Display TextField
-        self.address_text = CTkTextbox(
-            self.frame_1,
-            width=300,
-            height=100,
-        )
-        self.address_text.insert("0.0", "Ready" + '\n')
-        self.address_text.insert("0.0", "Ready" + '\n')
-        self.address_text.insert("0.0", "Ready" + '\n')
-        self.address_text.insert("0.0", "Ready" + '\n')
-        self.address_text.insert("0.0", "Ready" + '\n')
-        self.address_text.insert("0.0", "Ready" + '\n')
+            self.frame_1, text="ที่อยู่ ", width=70, anchor=tk.W)
+        self.address_label.grid(row=0, column=0, padx=(
+            0, 0), pady=(0, 5), sticky='nw')
 
+        self.address_text = CTkTextbox(self.frame_1, width=300, height=100)
+        self.address_text.insert("0.0", "Ready" + '\n')
         self.address_text.configure(state="disabled")
         self.address_text.grid(row=0, column=1, padx=(1, 0))
 
-        # * >Set Number Display Component ///////////////////1
-        # *> ContentsFrame
-        self.frame_1_1 = CTkFrame(
-            master=self.frame_1,
-            # padx=5, pady=5,
-            # bg_color="#a1a1a1" มันคือไอขอบๆมันเป็นขอบเพราะว่า style พื้นฐานมันมี borderradius ที่fgซึ่งเปนสีดำ พอ setbg มันจะเห็นสีของ bg เป็นขอบ
-        )
+        self.frame_1_1 = CTkFrame(master=self.frame_1)
         self.frame_1_1.grid(row=0, column=2, sticky='nw', padx=5)
 
         address_components_settings = [
-            {
-                "label": "จังหวัด",
-                "position": {"row": 0, "column": 1}},
-            {
-                "label": "อําเภอ/เขต",
-                "position": {"row": 0, "column": 3}},
-            {
-                "label": "ตำบล/แขวง",
-                "position": {"row": 0, "column": 5}},
-            {
-                "label": "รหัสไปรษณีย์",
-                "position": {"row": 1, "column": 1}},
-            {"label": "เบอร์โทร.",
-                "position": {"row": 1, "column": 3}},
+            {"label": "จังหวัด", "position": {"row": 0, "column": 1}},
+            {"label": "อําเภอ/เขต", "position": {"row": 0, "column": 3}},
+            {"label": "ตำบล/แขวง", "position": {"row": 0, "column": 5}},
+            {"label": "รหัสไปรษณีย์", "position": {"row": 1, "column": 1}},
+            {"label": "เบอร์โทร.", "position": {"row": 1, "column": 3}},
         ]
         for item in address_components_settings:
             # * >> Labels
@@ -257,51 +187,28 @@ class MainApp:
         ]
 
         for item in bottom_component_settings:
+            # * Frame1 Section /////////////////////////////////////////////////////////////////////////////
+            # * > address display Conponent /////////////////////
             # * >> Label
             self.address_label = CTkLabel(
-                self.frame_bottom,
-                text=item['label'],
-                width=70,
-                anchor=W
-            )
-            self.address_label.grid(
-                row=item['position']['row'],
-                column=item['position']['column'],
-                padx=(0, 5), pady=(0, 5),
-                sticky='nw'
-            )
+                self.frame_bottom, text=item['label'], width=70, anchor=tk.W)
+            self.address_label.grid(row=item['position']['row'], column=item['position']['column'], padx=(
+                0, 5), pady=(0, 5), sticky='nw')
             # * > DisplayField
             self.log_display = CTkTextbox(
-                self.frame_bottom,
-                height=150,
-                state=DISABLED)
+                self.frame_bottom, height=150, state=tk.DISABLED)
             self.log_display.grid(
-                row=item['position']['row']+1,
-                column=item['position']['column'],
-                padx=(0, 5), pady=(0, 5),
-                sticky='w')
+                row=item['position']['row'] + 1, column=item['position']['column'], padx=(0, 5), pady=(0, 5), sticky='w')
 
     def create_main_window(self):
         self.root.geometry("1000x450+400+300")
-
         self.root.title("Commart Autopage v0.1")
-
         # * use CANVAS as BG #################
-        self.canvas = CTkFrame(
-            master=self.root,
-            width=500,
-
-            corner_radius=0,
-
-        )
+        self.canvas = CTkFrame(master=self.root, width=500, corner_radius=0)
         self.canvas.pack(fill="both", expand=True, padx=5, pady=7)
-
         # * FRAMES ###########################
         # *> TopFrame
-        self.frame_top = CTkFrame(
-            master=self.canvas,
-
-        )
+        self.frame_top = CTkFrame(master=self.canvas)
         self.frame_top.pack(side='top', fill='x', padx=5, pady=7, anchor='w')
 
         # *> ContentsFrame
@@ -309,15 +216,21 @@ class MainApp:
             master=self.canvas,
             # bg_color="#a1a1a1" มันคือไอขอบๆมันเป็นขอบเพราะว่า style พื้นฐานมันมี borderradius ที่fgซึ่งเปนสีดำ พอ setbg มันจะเห็นสีของ bg เป็นขอบ
         )
+        self.frame_1 = CTkFrame(master=self.canvas)
         self.frame_1.pack(side='top', fill='x', padx=5, pady=7, anchor='w')
 
         # *> BottomFrame
         self.frame_bottom = CTkFrame(
             master=self.canvas,
         )
+        self.frame_bottom = CTkFrame(master=self.canvas)
         self.frame_bottom.pack(side='bottom', padx=5, pady=7, anchor='w')
 
         self.create_widgets()
+
+    def on_start_button_click(self):
+        thread = threading.Thread(target=self.start_task)
+        thread.start()
 
 
 def main():
@@ -326,8 +239,7 @@ def main():
         root.destroy()
 
     def ctrl_saraea_copy(event):
-        ctrl_state = event.state & 0x4 != 0  # 0x4 คือ flag สำหรับ Control key
-        # 67 คือรหัสสำหรับสระแอในภาษาไทย (อาจแตกต่างบนระบบอื่นๆ)
+        ctrl_state = event.state & 0x4 != 0
         if ctrl_state and event.keycode == 67:
             event.widget.event_generate("<<Copy>>")
 
@@ -351,6 +263,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# * hints
-# * ชื่อชุด kit กับ เลข set แยกกัน input
