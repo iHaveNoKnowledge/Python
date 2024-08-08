56f324ceeee39978f7effb53f9cd38af94c2fcb4
iHaveNoKnowledge <hackdk12@gmail.com>
2024-08-08 10:15:26 +0700

    save

---
+++ test/test_import_module/main.py
@@ -0,0 +1,8 @@
+# py2.py
+
+# นำเข้าฟังก์ชันจาก py1
+from sub1 import hello, add
+
+# ใช้ฟังก์ชันที่นำเข้ามา
+print(hello())  # จะพิมพ์ "Hello from py1"
+print(add(3, 5))  # จะพิมพ์ 8
---
+++ test/test_import_module/sub1.py
@@ -0,0 +1,8 @@
+# py1.py
+
+def hello():
+    return "Hello from py1"
+
+
+def add(a, b):
+    return a + b
