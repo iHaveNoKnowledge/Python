b0c13053b2bbc3789cbb1690ae4f5685b42ac0e5
iHaveNoKnowledge <hackdk12@gmail.com>
2025-12-22 18:05:03 +0700

    wip: readdress มันทำงานทุกชื่อใบกำกับ
    ยังหาสาเหตุไม่เจอ แต่ที่แน่ๆ current address ที่ได้จาก req มันเป็นค่าว่างทุกใบกำกับ

--- projects/auto_page/autopageMKII/autopage_MKII_ver4_1_0.py
+++ projects/auto_page/autopageMKII/autopage_MKII_ver4_1_0.py
@@ -5365,12 +5365,12 @@ class Bot_POS:
         self.origin = current_url.replace(matched_str, '')
 
         headers = {
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.9,th;q=0.8',
             'Connection': 'keep-alive',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'Origin': f'{self.origin}',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
             'X-Requested-With': 'XMLHttpRequest',
         }
 
