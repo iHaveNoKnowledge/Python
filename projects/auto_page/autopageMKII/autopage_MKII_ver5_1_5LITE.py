be013468f5f871bddae68716efc7cce902b0446e
iHaveNoKnowledge <hackdk12@gmail.com>
2026-05-19 16:29:26 +0700

    chore: ปรับชื่อไฟล์ และ update_note

--- projects/auto_page/autopageMKII/autopage_MKII_ver5_1_5LITE.py
+++ projects/auto_page/autopageMKII/autopage_MKII_ver5_1_5LITE.py
@@ -514,7 +514,7 @@ class MyApp:
         ))
 
         self.root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
-        self.root.title("Autosamatic ver5.1.4LITE(Hotfix)")
+        self.root.title("Autosamatic ver5.1.5LITE")
         self.root.configure(fg_color="#444")
 
         # กำหนด minimum size
